6c2540a1c04317a373b68761e2a46ef8d6398499
Alex <mazdarider23@gmail.com>
2016-09-12 17:52:36 -0400

    Disable dashboard suggestions [1/2]
    Also with:
    https://github.com/DirtyUnicorns/android_packages_apps_Settings/commit/580991a498e49a036aac3412d253829b2cb44199
    
    Change-Id: I6fab69dc56b29762add78fa7b118a63b8f08b32c
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- src/com/android/settings/dashboard/DashboardAdapter.java
+++ src/com/android/settings/dashboard/DashboardAdapter.java
@@ -118,12 +118,22 @@ public class DashboardAdapter extends RecyclerView.Adapter<DashboardAdapter.Dash
     }
 
     public List<Tile> getSuggestions() {
-        return mSuggestions;
+        if ((Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.DISABLE_SUGGESTIONS, 1) == 1)) {
+             return mSuggestions;
+        } else {
+             return null;
+        }
     }
 
     public void setCategoriesAndSuggestions(List<DashboardCategory> categories,
             List<Tile> suggestions) {
-        mSuggestions = suggestions;
+        if ((Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.DISABLE_SUGGESTIONS, 1) == 1)) {
+             mSuggestions = suggestions;
+        } else {
+             mSuggestions = null;
+        }
         recountItems();
     }
 
