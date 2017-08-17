AOSPA/android_packages_apps_Settings

Settings: Introduce PIE settings
This is a fragment which is only accessible
from the PIE snappoint.

Change-Id: Ida6a6737c97cfee3876f4623b5e6e4c24136cd03
Signed-off-by: Alex Naidis <alex.naidis@linux.com>

src/com/android/settings/SettingsActivity.java
@@ -392,7 +392,8 @@ public class SettingsActivity extends SettingsDrawerActivity
             ManageDomainUrls.class.getName(),
             AutomaticStorageManagerSettings.class.getName(),
             ButtonsSettings.class.getName(),
-            GesturesSettings.class.getName()
+            GesturesSettings.class.getName(),
+            PieSettings.class.getName()
     };
 
     private static final String[] LIKE_SHORTCUT_INTENT_ACTION_ARRAY = {

DesolationNougat/packages_apps_Settings

src/com/android/settings/SettingsActivity.java
@@ -409,7 +409,8 @@ public class SettingsActivity extends SettingsDrawerActivity
             FlingSettings.class.getName(),
             SmartbarSettings.class.getName(),
             PulseSettings.class.getName(),
-            QSSettings.class.getName()
+            QSSettings.class.getName(),
+            PieSettings.class.getName()
     };
 
 